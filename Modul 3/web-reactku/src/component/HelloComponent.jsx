19af6fd39a6a60397c38858af4e5aafb4299d53d
Rizqita Siska L. R <71862974+Qitaa@users.noreply.github.com>
2022-03-13 16:37:06 +0700

    minggu 2 & 3

---
+++ Modul 3/web-reactku/src/component/HelloComponent.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import './HelloComponent.css';
+
+const HelloComponent = () =>{
+    return <p className='text-p'> Ini adalah arrow function</p>
+}
+
+export default HelloComponent;
